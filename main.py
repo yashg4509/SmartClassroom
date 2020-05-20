6bd2122b9fa4e8ed05e083320e5016b9789f95e8
Saber Nomen <genericp3rson@gmail.com>
2020-05-20 11:41:40 -0500

    Skeleton of Flask

--- main.py
+++ main.py
@@ -1,14 +1,36 @@
-from flask import Flask
-from flask_socketio import SocketIO, send
+from flask import Flask, render_template
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'mysecret'
-socketio = SocketIO(app)
+app = Flask(__name__, static_url_path="/static")
 
-@socketio.on('message')
-def handleMessage(msg):
-    print('Message: ' + msg)
-    send(msg, broadcast = True)
+@app.route("/")
+def main():
+    '''
+    Login and Signup
+    '''
+    return render_template('index.html')
 
-if (__name__ == '__main__'):
-    socketio.run(app)
+@app.route("/chat")
+def chat():
+    return render_template('chat.html')
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
+# from flask import Flask
+# from flask_socketio import SocketIO, send
+
+# app = Flask(__name__)
+# app.config['SECRET_KEY'] = 'mysecret'
+# socketio = SocketIO(app)
+
+# @socketio.on('message')
+# def handleMessage(msg):
+#     print('Message: ' + msg)
+#     send(msg, broadcast = True)
+
+# if (__name__ == '__main__'):
+#     socketio.run(app)
